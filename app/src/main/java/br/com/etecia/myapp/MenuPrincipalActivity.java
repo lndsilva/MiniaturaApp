49b47a9d17abdc8ef1b93af02df4e82c64993f7f
lndsilva <lndsilva@hotmial.com>
2025-08-22 19:55:03 -0300

    miniatura

--- app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
@@ -1,6 +1,9 @@
 package br.com.etecia.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
+
+    ImageView imgMenuPrincipal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,16 @@ public class MenuPrincipalActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        imgMenuPrincipal = findViewById(R.id.imgMenuPrincipal);
+
+        imgMenuPrincipal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),
+                        ItensMiniaturaActivity.class));
+            }
+        });
+
     }
 }
