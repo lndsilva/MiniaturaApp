5fce422f2b6ed792ec5164b999b9a08c7f041a21
lndsilva <lndsilva@hotmial.com>
2025-08-22 20:13:34 -0300

    miniatura

--- app/src/main/java/br/com/etecia/myapp/ItensMiniaturaActivity.java
+++ app/src/main/java/br/com/etecia/myapp/ItensMiniaturaActivity.java
@@ -1,6 +1,8 @@
 package br.com.etecia.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.appbar.MaterialToolbar;
+
 public class ItensMiniaturaActivity extends AppCompatActivity {
+
+    MaterialToolbar idToolBarItens;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,17 @@ public class ItensMiniaturaActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        idToolBarItens = findViewById(R.id.idToolBarItens);
+
+        idToolBarItens.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),
+                        MenuPrincipalActivity.class));
+            }
+        });
+
+
     }
 }
