90c37ea4b9363201be7270cdf493acdc1cc8c934
lndsilva <lndsilva@hotmial.com>
2025-08-22 22:17:35 -0300

    adaptador

--- app/src/main/java/br/com/etecia/myapp/ItensMiniaturaActivity.java
+++ app/src/main/java/br/com/etecia/myapp/ItensMiniaturaActivity.java
@@ -45,6 +45,27 @@ public class ItensMiniaturaActivity extends AppCompatActivity {
             }
         });
 
+        AdapterItensMiniatura adapterItensMiniatura = new AdapterItensMiniatura(this);
+
+        idViewPagerItensMiniatura.setAdapter(adapterItensMiniatura);
+
+        idTabLayoutItensMiniatura.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                idViewPagerItensMiniatura.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
 
 
     }
