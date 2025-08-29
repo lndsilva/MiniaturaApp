df1324844e38ccde3d627f1e0343a5988ab08c83
lndsilva <lndsilva@hotmail.com>
2025-08-29 20:23:02 -0300

    fragment

---
+++ app/src/main/java/br/com/etecia/myapp/AdaptadorMiniatura.java
@@ -0,0 +1,43 @@
+package br.com.etecia.myapp;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class AdaptadorMiniatura extends RecyclerView.Adapter<AdaptadorMiniatura.ViewHolder> {
+    private Context context;
+    private List<Miniatura> lstMiniatura;
+
+    public AdaptadorMiniatura(Context context, List<Miniatura> lstMiniatura) {
+        this.context = context;
+        this.lstMiniatura = lstMiniatura;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
